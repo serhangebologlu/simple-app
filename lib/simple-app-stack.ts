eb0984877ccfad8866391b3f2904e16c4161a505
serhan <sgebologlu@gmail.com>
2021-06-05 21:01:50 +0300

    lambda stack ekleme

--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,5 +1,9 @@
 import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 import * as cdk from '@aws-cdk/core';
+import { Runtime } from '@aws-cdk/aws-lambda';
+import * as path from 'path';
+
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -8,6 +12,12 @@ export class SimpleAppStack extends cdk.Stack {
     const bucket = new Bucket(this, 'MySimpleAppBucket', {
       encryption: BucketEncryption.S3_MANAGED,
     });
+
+    const getPhotos = new lambda.NodejsFunction(this, 'MySimpleAppLambda', {
+      runtime: Runtime.NODEJS_12_X,
+      entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
+      handler: 'getPhotos',
+    }) 
 
     new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
       value: bucket.bucketName,
